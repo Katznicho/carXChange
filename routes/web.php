0bd00ad46b41deb5ddc58c97158251fc86b333a3
katznicho <katznicho@gmail.com>
2025-04-19 11:49:17 +0300

    wip:added examples for car and car details

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,23 @@ use App\Livewire\Site\WebHomePage;
 use App\Models\Car;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', WebHomePage::class)->name('site.index');
 Route::get('/details', WebCarDetails::class)->name('site.car.details');
 
+// Get all cars
+Route::get('/cars', function () {
+    $cars = Car::with(['category', 'dealer'])
+        ->where('is_active', true)
+        ->latest()
+        ->get();
+    
+    return response()->json([
+        'status' => 'success',
+        'data' => $cars->map->formatted_data
+    ]);
+})->name('cars.index');
+
+// Get single car details
 Route::get('/cars/{car}', function (Car $car) {
     $car->load(['dealer', 'category', 'reviews' => function($query) {
         $query->where('is_approved', true);
